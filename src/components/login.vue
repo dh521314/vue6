70d8f3b2c00a155062b95912d2058c05684c4070
cs <2931119681@qq.com>
2020-08-26 19:54:09 +0800

    login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -22,100 +22,95 @@
   </div>
 </template>
 <style lang="scss">
-._login {
-  // border:1px solid red;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .login_inner {
-    // border:1px solid green;
-    width: 460px;
-    height: 300px;
-    margin-top: -150px;
+  ._login {
+    // border:1px solid red;
+    height: 100%;
+    width: 100%;
     display: flex;
+    align-items: center;
     justify-content: center;
-    box-shadow: 0 0 25px #cac6c6;
-    .form {
-      // border:1px solid blue;
-      width: 300px;
-      margin-top: 30px;
-      text-align: center;
-      .title {
-        color: #505458;
+
+    .login_inner {
+      // border:1px solid green;
+      width: 460px;
+      height: 300px;
+      margin-top: -150px;
+      display: flex;
+      justify-content: center;
+      box-shadow: 0 0 25px #cac6c6;
+
+      .form {
+        // border:1px solid blue;
+        width: 300px;
+        margin-top: 30px;
+        text-align: center;
+
+        .title {
+          color: #505458;
+        }
       }
-    }
-    .error {
-      color: red;
+
+      .error {
+        color: red;
+      }
     }
   }
-}
 </style>
 <script>
-export default {
-  name: "login",
-  data() {
-    return {
-      formLogin: {
-        loginName: "daihui",
-        password: "66668888"
-      },
-      errorInfo: {
-        text: "登陆失败,请重试",
-        isShowError: false //显示错误提示
-      }
-    };
-  },
-   mounted() {
-        document.onkeydown = (event) => {
-            var router=this.$route.path;
-            var e = event || window.event || arguments.callee.caller.arguments[0];
-            if (e && e.keyCode == 13&&this.$route.path=='/login') { // enter 键
-                this.login();
-            }
-        };
-    },
-  methods: {
-    login() {
-      var param = {
-        loginName: this.formLogin.loginName,
-        password: this.formLogin.password
+  export default {
+    name: "login",
+    data() {
+      return {
+        formLogin: {
+          loginName: "daihui",
+          password: "66668888"
+        },
+        errorInfo: {
+          text: "登陆失败,请重试",
+          isShowError: false //显示错误提示
+        }
       };
-      this.$http
-        .post(`http://localhost:8088/aiyue/Emp/login?ename=${this.formLogin.loginName}&epwd=${this.formLogin.password}`)
-        .then(response => {
-          console.log("成功报文:", response);
-          var json = response.data;
-          if (json.length > 0) {
-            var userInfo = json[0];
-            var position = "";
-            this.$http.post("http://localhost:8088/aiyue/Post/findByPid?pid="+userInfo.postid)
-              .then((resp)=>{
-                position = resp.data;
-              });
+    },
+    mounted() {
+      document.onkeydown = (event) => {
+        var router = this.$route.path;
+        var e = event || window.event || arguments.callee.caller.arguments[0];
+        if (e && e.keyCode == 13 && this.$route.path == '/login') { // enter 键
+          this.login();
+        }
+      };
+    },
+    methods: {
+      login() {
+        this.$http
+          .post(`http://localhost:8088/aiyue/Emp/login?ename=${this.formLogin.loginName}&epwd=${this.formLogin.password}`)
+          .then(response => {
+            console.log("成功报文:", response);
+            var json = response.data;
+            if (json.length > 0) {
+              var userInfo = json[0];
+              var position = json[1];
 
-            setTimeout(function(){
               //保存登陆信息
               sessionStorage.setItem("userName", userInfo.ename); //用户名
               sessionStorage.setItem("token", JSON.stringify(userInfo)); //保存用户
 
               sessionStorage.setItem("position", position); //用户职位
               //登陆成功跳转主页
-            },2000);
-            this.$router.replace({ path: "/index" });
+              this.$router.replace({path: "/index"});
 
-          } else {
+            } else {
+              this.errorInfo.isShowError = true;
+              this.errorInfo.text = "用户名或者密码错误";
+            }
+          })
+
+          .catch(error => {
+            console.log("失败报文:", error);
             this.errorInfo.isShowError = true;
-            this.errorInfo.text = "用户名或者密码错误";
-          }
-        })
-        .catch(error => {
-          console.log("失败报文:", error);
-          this.errorInfo.isShowError = true;
-          this.errorInfo.text = "系统接口异常";
-        });
+            this.errorInfo.text = "系统接口异常";
+          });
+      }
     }
-  }
-};
+  };
 </script>
