ef0893436124561982c9bd853fa2a432db0ae223
han123777 <1378009552@qq.com>
2020-08-27 19:13:32 +0800

    writer

--- src/components/admin/writer.vue
+++ src/components/admin/writer.vue
@@ -12,6 +12,21 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <div>
+      <!--分页-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[2,10,12]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="parseInt(total)">
+      </el-pagination>
+
+    </div>
+
     <el-dialog :title="title" :visible.sync="dialogFormVisible">
       <el-form :model="writer" label-width="100px">
         <el-form-item label="编号" hidden="hidden">
@@ -43,18 +58,24 @@
           dialogFormVisible:false,
           writer:{},
           title:'',
-          list:[]
+          list:[],
+          // 分页用到的数据
+          currentPage: 1,
+          pageSize: 10,
+          total: 0
         }
       },
       created:function () {
-        this.query();
+        this.query(1,10);
       },
       methods:{
-        query:function(){
-          this.$http.post('http://localhost:8088/aiyue/writer/writerQuery')
+        query:function(num,size){
+          this.$http.post('http://localhost:8088/aiyue/writer/pageFind?num='+num+'&size='+size)
             .then(res=>{
               console.info(res)
-              this.list=res.data;
+              this.list=res.data.list;
+              this.total = res.data.total;
+              this.currentPage = res.data.pageNum;
             });
         },
         show:function (row) {
@@ -73,7 +94,7 @@
             this.$http.post(`http://localhost:8088/aiyue/writer/addWriter?wname=${this.writer.wname}&wphoto=${this.writer.wphoto}&ana=${this.writer.ana}`)
               .then((resp)=>{
                 if(resp.data == 1) {
-                  this.query();
+                  this.query(1,this.pageSize);
                 }else if(resp.data == 2){
                   this.$message.info("已存在");
                 }else{
@@ -81,17 +102,41 @@
                 }
              })
           }else {
-            this.$http.post(`http://localhost:8088/aiyue/writer/updateWriter?wid=${this.writer.wid}&wname=${this.writer.wname}&wphoto=${this.writer.wphoto}&ana=${this.writer.ana}`)
-              .then((resp) => {
-                if(resp.data == 1) {
-                  this.query();
-                }else if(resp.data == 2){
-                  this.$message.info("已存在");
-                }else{
-                  this.$message.info("修改失败");
+            this.$http.post(`http://localhost:8088/aiyue/writer/QueryByWriterName?wname=${this.writer.wname}`)
+              .then((res) => {
+                if (res.data.wname == null){
+                  this.$http.post(`http://localhost:8088/aiyue/writer/updateWriters?wid=${this.writer.wid}&wname=${this.writer.wname}&wphoto=${this.writer.wphoto}&ana=${this.writer.ana}`)
+                    .then((resp) => {
+                      if(resp.data == 1) {
+                        this.$message.info("修改成功");
+                        this.query(1,this.pageSize);
+                      }else {
+                        this.$message.info("修改失败");
+                      }
+                    })
+                } else{
+                  this.$http.post(`http://localhost:8088/aiyue/writer/updateWriter?wid=${this.writer.wid}&wphoto=${this.writer.wphoto}&ana=${this.writer.ana}`)
+                    .then((resps) => {
+                      if(resps.data == 1) {
+                        this.$message.info("作家已存在或未修改作家名")
+                        this.query(1,this.pageSize);
+                      }else {
+                        this.$message.info("修改失败");
+                      }
+                    })
                 }
               })
            }
+        },
+        handleSizeChange:function(newSize){
+          // pagesize改变触发
+          this.pageSize = newSize;
+          this.query(this.currentPage,this.pageSize)
+        },
+        handleCurrentChange(newPage) {
+          // 页码改变触发
+          this.currentPage = newPage;
+          this.query(this.currentPage,this.pageSize)
         }
       }
     }
